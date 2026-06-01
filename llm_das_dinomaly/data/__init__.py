49d8f62ab8e1f9318d8a1e0d56ca6277961ea7d6
xtxtxt2568 <14284183+xtxtxt2568@user.noreply.gitee.com>
2026-06-01 12:14:31 +0800

    feat: index mvtec test split

--- llm_das_dinomaly/data/__init__.py
+++ llm_das_dinomaly/data/__init__.py
@@ -1,11 +1,19 @@
 """Data and cache helpers."""
 
 from llm_das_dinomaly.data.cache import load_tensor_cache, save_tensor_cache, save_torch_payload
-from llm_das_dinomaly.data.mvtec import MVTecGoodDataset, MVTecImageRecord, list_mvtec_train_good
+from llm_das_dinomaly.data.mvtec import (
+    MVTecGoodDataset,
+    MVTecImageRecord,
+    MVTecTestDataset,
+    list_mvtec_test_images,
+    list_mvtec_train_good,
+)
 
 __all__ = [
     "MVTecGoodDataset",
     "MVTecImageRecord",
+    "MVTecTestDataset",
+    "list_mvtec_test_images",
     "list_mvtec_train_good",
     "load_tensor_cache",
     "save_tensor_cache",
